0d87ca3a59c15ad56dec96d430aae1481c512f52
Jack Brassil <43377162+jackbrassil@users.noreply.github.com>
2025-02-14 12:24:44 -0500

    Add files via upload

---
+++ invertMatrix/concurrent-inversions.sh
@@ -0,0 +1,4 @@
+(time python invertMatrix_CPU0GPU0_96.py) >> 96inverts-out.txt 2>&1
+(time python invertMatrix_CPU0GPU0_96.py) >> 96inverts-out.txt 2>&1
+(time python invertMatrix_CPU0GPU0_96.py) >> 96inverts-out.txt 2>&1
+lscpu >> 96inverts-out.txt 2>&1
